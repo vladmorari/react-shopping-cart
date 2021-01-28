65d41e55b0b053acf275ab73e8cd8576d72bd6d7
vladmorari <morariv67@gmail.com>
2021-01-28 17:50:23 +0200

    Products component ready

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
-import 
+import { ProductContext } from "./contexts/ProductContext";
 
 // Components
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
-
 
 function App() {
   const [products] = useState(data);
@@ -16,17 +15,20 @@ function App() {
   const addItem = (item) => {
     // add the given item to the cart
     console.log("item ", item);
-	setCart(cart => [...cart, item]);
+    setCart((cart) => [...cart, item]);
   };
   console.log("data ", cart);
   return (
+    //  <Products products={products} addItem={addItem} />
     <div className="App">
       <Navigation cart={cart} />
 
       {/* Routes */}
 
       <Route exact path="/">
-        <Products products={products} addItem={addItem} />
+        <ProductContext.Provider value={{ prod: products, fun: addItem }}>
+          <Products />
+        </ProductContext.Provider>
       </Route>
 
       <Route path="/cart">
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,19 @@
-import React from 'react';
-
+import React from "react";
+import { ProductContext } from "../contexts/ProductContext";
+import { useContext } from "react";
 // Components
-import Product from './Product';
+import Product from "./Product";
 
-const Products = props => {
-	return (
-		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
-			))}
-		</div>
-	);
+const Products = () => {
+  const value = useContext(ProductContext);
+  console.log(">>>>",value.prod)
+  return (
+    <div className="products-container">
+      {value.prod.map((product) => (
+        <Product key={product.id} product={product} addItem={value.fun} />
+      ))}
+    </div>
+  );
 };
 
 export default Products;
